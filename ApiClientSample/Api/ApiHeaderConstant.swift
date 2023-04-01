b3fc3e1bf01c7db0e03eac3b7704259f5366d227
Hikaru Kurodq <h1kqaa@gmail.com>
2023-04-01 22:10:11 +0900

    コメント追加

--- ApiClientSample/Api/ApiHeaderConstant.swift
+++ ApiClientSample/Api/ApiHeaderConstant.swift
@@ -10,6 +10,7 @@ import Foundation
 class ApiHeaderConstant {
     
     static let qiita: [String: String] = [
+        // Qiitaの認証トークンはGit管理から除外
         "Authorization": "Bearer \(ApiKey.qiitaApiToken)",
         "Content-Type": "application/json"
     ]
